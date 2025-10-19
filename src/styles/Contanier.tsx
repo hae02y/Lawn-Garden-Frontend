dc182bebf931be0e2e552a37cdeac30e672b5a65
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-10-19 16:07:34 +0900

    feat : 전역으로 사용할 Contanier, FooterPagination styles 정의

---
+++ src/styles/Contanier.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Container = styled.section`
+  background-color: var(--color-container-background);
+  border-radius: 30px;
+  width: 90vw;
+  max-width: 770px;
+  padding: 1.2rem 1.2rem 1.2rem 1.2rem;
+  margin-top: 1rem;
+  min-height: 505px;
+`;
+
+export default Container;
